7788968337bf8ac3a36c037d4e47743773cc6b21
Dmitry <zapdmitry@gmail.com>
2018-03-16 15:21:25 +0300

    Add task_3 with tests

---
+++ Task_05/task_DEV3/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace task_DEV3
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NumberBaseConverter radixChanger = new NumberBaseConverter();
+                BigInteger inputNumber = BigInteger.Parse(args[0]);
+                int newBase = Int16.Parse(args[1]);
+                Console.WriteLine(radixChanger.GetNewNumberPresentation(inputNumber, newBase));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
