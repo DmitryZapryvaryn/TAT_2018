27f15d0ab0db0946b1d5200c18b79ac5a275d2da
Dmitry <zapdmitry@gmail.com>
2018-03-16 04:21:36 +0300

    Add task_2 with tests

---
+++ Task_05/task_DEV2/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_DEV2
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            EvenCharStringCreator evenChar = new EvenCharStringCreator();
+            string buffer = Console.ReadLine();
+            Console.WriteLine(evenChar.GetEvenCharSequence(buffer));
+        }
+    }
+}
